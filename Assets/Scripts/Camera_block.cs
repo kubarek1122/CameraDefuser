b5f58516d4cc8d843908a52e81b88a74abe03cba
Mateusz Majchrzak <maateuszmajchrzak@gmail.com>
2021-11-03 20:37:18 +0100

    Checkpoint, daying (returning to checkpoint), building level, work on colliding camera

---
+++ Assets/Scripts/Camera_block.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_block : MonoBehaviour
+{
+
+    public GameObject main_camera;
+
+
+
+    void Start()
+    {
+
+        
+
+    }
+
+    void Update()
+    {
+        //this.transform.position = main_camera.transform.position;
+    }
+
+
+
+}
---
+++ Assets/Scripts/Player_camera_block.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_camera_block : MonoBehaviour
+{
+
+    //private Transform _target;
+    //private float _distanceFromTarget = 3.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      //  transform.position = _target.position + transform.forward * _distanceFromTarget;
+
+    }
+}
