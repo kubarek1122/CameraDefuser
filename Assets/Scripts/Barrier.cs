34fbcfc1b923c4ee5144f0562af529cb7b321a83
kubarek1122 <kubarek1122@gmail.com>
2021-11-07 12:56:43 +0100

    Added barrires in doors with custom shader and script

---
+++ Assets/Scripts/Barrier.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Barrier : MonoBehaviour
+{
+    [SerializeField] private BoxCollider _door;
+    
+    [SerializeField] private Material _material;
+
+    private Vector4 _color;
+    void Start()
+    {
+        _color = _material.GetVector("Color_d9dfc07cd944497c94d6401352b43c55");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            _material.SetVector("Color_d9dfc07cd944497c94d6401352b43c55", new Vector4(0f, 3.5f, 0, 0));
+            _door.enabled = false;
+        }
+
+        if (other.tag == "MainCamera")
+        {
+            _material.SetVector("Color_d9dfc07cd944497c94d6401352b43c55", new Vector4(3.5f, 0, 0, 0));
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        _material.SetVector("Color_d9dfc07cd944497c94d6401352b43c55", _color);
+        _door.enabled = true;
+    }
+}
