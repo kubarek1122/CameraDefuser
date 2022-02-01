268a68aa50cb423fb4ea04ae14cf2a1cd295803a
kubarek1122 <kubarek1122@gmail.com>
2022-02-01 13:26:18 +0100

    detecting walls

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,15 +6,63 @@ public class CameraController : MonoBehaviour
 {
     [SerializeField] private ThirdPersonMovement player;
     [SerializeField] private Animation anim;
+
+    [SerializeField] float topDetection = 1.0f,
+                            bottomDetection = 1.0f,
+                            backDetection = 1.0f,
+                            diagonalDetection = 1.0f;
+
+    [SerializeField] private CanvasGroup top, bottom, back;
+
+    [SerializeField] LayerMask wallsLayer;
     void Start()
     {
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
+
+        top.alpha = 0;
+        bottom.alpha = 0;
+        back.alpha = 0;
     }
 
     void Update()
     {
-        
+        bool top_check = Physics.Raycast(transform.position, transform.up, topDetection, wallsLayer);
+        bool bottom_check = Physics.Raycast(transform.position, -transform.up, bottomDetection, wallsLayer);
+        bool back_check = Physics.Raycast(transform.position, -transform.forward, backDetection, wallsLayer);
+        bool topback_check = Physics.Raycast(transform.position, (transform.up - transform.forward).normalized, diagonalDetection, wallsLayer);
+        bool bottomback_check = Physics.Raycast(transform.position, (-transform.up - transform.forward).normalized, diagonalDetection, wallsLayer);
+
+        top.alpha = 0;
+        bottom.alpha = 0;
+        back.alpha = 0;
+
+        if (top_check)
+        {
+            top.alpha = 1;
+        }
+
+        if (bottom_check)
+        {
+            bottom.alpha = 1;
+        }
+
+        if(back_check)
+        {
+            back.alpha = 1;
+        }
+
+        if (topback_check)
+        {
+            top.alpha = 1;
+            back.alpha = 1;
+        }
+
+        if (bottomback_check)
+        {
+            bottom.alpha = 1;
+            back.alpha = 1;
+        }
     }
 
     private void OnTriggerEnter(Collider other)
