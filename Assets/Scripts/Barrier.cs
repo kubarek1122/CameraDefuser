a37a6df5493ac4ae10a86850203fcaa9df9e1a78
kubarek1122 <kubarek1122@gmail.com>
2022-05-28 16:22:53 +0200

    Updated version, fixed physics related problem, fixed barrier

--- Assets/Scripts/Barrier.cs
+++ Assets/Scripts/Barrier.cs
@@ -24,6 +24,7 @@ public class Barrier : MonoBehaviour
         if (other.tag == "MainCamera")
         {
             _material.SetVector("Color_d9dfc07cd944497c94d6401352b43c55", new Vector4(3.5f, 0, 0, 0));
+            _door.enabled = true;
         }
     }
 
